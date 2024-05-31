469390c48ef6c0315fb2a8c4813c8a470dcc1630
Aryan Malik <121257483+Mr-Malik-Aryan@users.noreply.github.com>
2024-05-31 11:16:01 +0530

    Update upload.js

--- src/components/upload.js
+++ src/components/upload.js
@@ -35,7 +35,7 @@ export default function Upload() {
         setLoader("flex")
            // https://fish-it-backend.onrender.com/upload
     try {
-      const responce = await axios.post("http://localhost:8080/upload",data)
+      const responce = await axios.post("http://13.202.121.242:8080/upload",data)
      alert(responce.data)
      setLoader("none")
     } catch (error) {
